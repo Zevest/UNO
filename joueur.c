687a9271fe908cb3b6976b36f2afa1ec455e45f3
Zevest <steveDP32014@gmail.com>
2020-02-03 15:18:37 +0100

    test main

--- joueur.c
+++ joueur.c
@@ -5,17 +5,25 @@
 #define Nombre_carte 7
  struct joueur
  {
- 	char nom ;
+ 	char * nom ;
  	pile_t * carte ; 
  };
 typedef struct joueur joueur_t;
 
-void  init_joueur(joueur_t * joueurs, int nb , char ** noms, pile_t * paquet){
-	int i ; 
-	joueurs = malloc(nb * sizeof * joueurs);
+joueur_t *  init_joueur(int nb , char * noms[], pile_t * paquet){
+	int i,j ; 
+	joueur_t * joueurs = malloc(nb * sizeof *joueurs);
 	for (i = 0; i < nb; ++i)
 	{
 		joueurs[i].nom = noms[i]; 
+		joueurs[i].carte = init_pile();
+		//printf("%s\n", noms[i]);
+		
 		distribuer(Nombre_carte, paquet, joueurs[i].carte); 
+		//printf("nb cartes restante : %d ",  paquet->_TOP+1);
+		/*for(j = 0; j < joueurs[i].carte->_TOP+1;++j){
+			printf("%s\n", SYMBOL[joueurs[i].carte->_DATA[j]->num]);
+		}*/
 	}
+	return joueurs;
 }
--- main.c
+++ main.c
@@ -2,79 +2,70 @@
 #include <stdlib.h>
 #include "carte.h"
 #include "pile.h"
+#include "joueur.h"
+#include <time.h>
 
+#define W 30
+#define H 15
 
-int max(int a, int b){
-	return a > b ? a: b;
-}
+static int sens;
+static int tour;
 
-void melange(pile_t * paquet, pile_t * res, int premier){
-	int useless;
-	if(!empty(res)){
-		//vider le paquet res
-		while(!empty(res)){
-			pop(res);
-		}
-	}else{
-		int mx = paquet->_TOP;
-		for(useless = 0; useless <= mx; ++useless){
-
-			int index = rand() % max(1, (paquet->_TOP-premier));
-			push(res, paquet->_DATA[index]);
-			carte_remove(paquet, index);
-		}
+void affiche_table(pile_t * paquet, joueur_t * joueur){
+	int j,i;
+	for(i = 0; i < W; ++i){
+		printf("#");
 	}
+	printf("\n");
+	for(j = 0; j < H; ++j){
+		printf("\n");
+	}
+	
 }
+
+void next(){
+	if(sens)
+		++tour;
+	else
+		--tour;
+}
+
+void inverse(){
+	return;
+}
+
+
 
 int main(){
-	pile_t * paquet = init_pile();
 	int i, j = 0;
-	for(i = 0; i <= 12; ++i){
-		if(i != 0){
-			push(paquet,init_carte(i, 'R'));
-			j++;
-			push(paquet,init_carte(i, 'B'));
-			j++;
-			push(paquet,init_carte(i, 'J'));
-			j++;
-			push(paquet,init_carte(i, 'V'));
-			j++;
-		}
-		push(paquet,init_carte(i, 'R'));
-		j++;
-		push(paquet,init_carte(i, 'B'));
-		j++;
-		push(paquet,init_carte(i, 'J'));
-		j++;
-		push(paquet,init_carte(i, 'V'));
-		j++;
-	}
-	for(i = 13; i < 15; ++i){
-		push(paquet,init_carte(i, 'N'));
-		j++;
-		push(paquet,init_carte(i, 'N'));
-		j++;
-		push(paquet,init_carte(i, 'N'));
-		j++;
-		push(paquet,init_carte(i, 'N'));
-		j++;
-	}
-	
+
+	//srand(time(NULL));
+	pile_t * paquet = init_pile();
+	remplir_paquet(paquet);
 	pile_t * pioche = init_pile();
-	melange(paquet, pioche, 0);
+	melange(paquet, pioche, 1);
 	
-	printf("\n\n%d  paquet\n", j);
-	for(i = 0; i < paquet->_TOP+1; ++i){
-		printf("%s %c\n", paquet->_DATA[i]->symbol, paquet->_DATA[i]->color);
-	}
-	printf("\npioche\n");
-	for(i = 0; i < pioche->_TOP-3; ++i){
-		printf("%s %c\n", pioche->_DATA[i]->symbol, pioche->_DATA[i]->color);
+	char * noms[] = {"Idir", "Mounir", "Karim", "Steve", "Rachid"};
+	
+	joueur_t * joueurs = init_joueur(5, noms, pioche);
+	
+	
+	for(i = 0; i < 5; ++i){
+		printf("%s\n", noms[i]);
+		for(j = 0; j < joueurs[i].carte->_TOP+1;++j){
+		
+			affiche_carte(joueurs[i].carte->_DATA[j]);
+		}
+		printf("\n");
 	}
 	
-	//carte_t *  c = init_carte(13, 'R');
-	//printf("\ncarte %c %s\n", c->color, c->symbol);
-	//free(c);
+	for(i = 0; i < 4; ++i){
+		free(joueurs[i].carte);
+	}
+	free(joueurs);
+	free(paquet);
+	free(pioche);
+	//free(jeux1);
 	return 0;
 }
 
