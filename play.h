f95a168430d39f795b6b5ec48bec75e8e185236f
COURAM STEVE <scouram@tosakin.bocal.cs.univ-paris8.fr>
2020-02-04 17:37:50 +0100

    ajout fonction ask

--- play.h
+++ play.h
@@ -15,3 +15,5 @@ int can_play_cart(carte_t * carte_A , carte_t * carte_B);
 int can_play(pile_t * main , carte_t * carte);
 
 void play(joueur_t *joueurs , pile_t * paquets , pile_t * pioches, int);
+
+int ask(int * v, char * message, char* message2);
